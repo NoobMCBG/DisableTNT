006177a7965bd3b1f0f8ff0ca4cfca720fbac92f
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2021-09-28 20:22:17 +0700

    Update Main.php

--- src/DisableTNT/Main.php
+++ src/DisableTNT/Main.php
@@ -10,12 +10,12 @@ class Main extends PluginBase implements Listener{
 	
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info(TextFormat::GREEN . "Plugin Đã Được Kích Hoạt");
+		$this->getLogger()->info(TextFormat::GREEN . "Enable Plugin");
 	}
 	public function onExplosionPrime(ExplosionPrimeEvent $event){
 		$event->setBlockBreaking(false);
 	}
 	public function onDisable(){
-		$this->getLogger()->info(TextFormat::RED . "Plugin Đã Bị Vô Hiệu Hóa Do Bạn Dừng Máy Chủ Hoặc Có Lỗi Phát Sinh");
+		$this->getLogger()->info(TextFormat::RED . "Disable Plugin");
 	}
 }
